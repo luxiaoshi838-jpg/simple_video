57da8fa069bc595b5f498ed31b2289cb2f85895b
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-09-04 18:08:18 +0800

    简播1.0.11：提升覆盖升级版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.luxiaoshi.jianbo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.11"
         vectorDrawables { useSupportLibrary = true }
     }
 
