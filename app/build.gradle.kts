a5e65ac49505641fcb97785dee3f8fb709904b8d
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-09-22 00:01:37 +0800

    Bump 简播 to 1.0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.luxiaoshi.jianbo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
         vectorDrawables { useSupportLibrary = true }
     }
 
